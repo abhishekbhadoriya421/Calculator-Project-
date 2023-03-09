b981e34914475d62a558e78094f6e229aae809e1
abhishekbhadoriya421 <127028405+abhishekbhadoriya421@users.noreply.github.com>
2023-03-10 00:39:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Calling DOM
 let buttons = document.querySelectorAll(".BTS p");
 let firstInputContainer = document.querySelector("#secondValue input");
+let firstValue = document.getElementById("firstValue");
 
 // Function Of Performing Calculation 
 function Calculation(num1, num2 , operator){
-    console.log(num1,operator,num2);
     if(operator=='+'){
         return num1+num2;
     }else if(operator=='-'){
@@ -24,27 +24,32 @@ let operator = "";
 let secondOperand = 0;
 // Function Which is Used For Finding And Storing the Pressed Button Value into Variable(tempValue) Which is In String Formate 
 function pressedBTN(element){
-    let num = element.srcElement.textContent; 
-    if(num>=0 && num<=9){
+    let num = element.srcElement.textContent;
+    
+    if(num>=0 && num<=9){ // if num is from 0 to 9 than it is number. so we are here storing numbers in String(tempValue variable)
         tempValue += num;
-        // console.log(tempValue);
         firstInputContainer.value = tempValue;
-    }
-    else if(num=='.'){
+        
+    }else if(num=='.'){ // here we considering the decimal values 
         tempValue+='.';
-        firstInputContainer.value = tempValue
+        firstInputContainer.value = tempValue;
+        
     }else if(num=="AC"){
-
         firstOperand = 0;
         secondOperand = 0;
         operator = "";
         tempValue = "";
         firstInputContainer.value = firstOperand;
+        firstValue.textContent = "";
+        
     }else if(num=="="){
         if(firstOperand==0 || tempValue==""){
             firstOperand = 0;
             tempValue = "";
+            operator = "";
+            secondOperand = 0;
             firstInputContainer.value = firstOperand;
+            firstValue.textContent = "";
         }
         else{
             secondOperand = Number(tempValue);
@@ -52,10 +57,13 @@ function pressedBTN(element){
             firstOperand = Ans;
             secondOperand = 0;
             operator = "";
-            tempValue = "";
+            tempValue = Ans;
             firstInputContainer.value = Ans;
+            firstValue.textContent = "";
         }
+        
     }else if(tempValue!=""){
+        
         if(operator!=""){
             secondOperand = Number(tempValue);
             let ans = Calculation(firstOperand,secondOperand,operator);
@@ -68,32 +76,24 @@ function pressedBTN(element){
         }
         
         tempValue = "";
-        
         if(num == '+'){
             operator = '+';
         }
-
         if(num == '-'){
             operator = '-';
         }
-
         if(num == '*'){
             operator = '*';
         }
-
         if(num == '/'){
             operator = '/';
         }
-
         if(num == '%'){
             operator = '%';
         } 
-        firstInputContainer.value = firstOperand;  
+        firstInputContainer.value = 0; 
+        firstValue.textContent = (firstOperand + num);
     }
-
-    
-    
-
 }
 
 buttons.forEach(btn => {
